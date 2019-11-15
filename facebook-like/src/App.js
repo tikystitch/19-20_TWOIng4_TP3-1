d0a8b66f6e3aeeb0e28a84eb8da425a4fed464bd
tikystitch <36538825+tikystitch@users.noreply.github.com>
2019-11-15 06:16:29 +0100

    changement UI + color done

--- facebook-like/src/App.js
+++ facebook-like/src/App.js
@@ -1,70 +1,171 @@
 import React from 'react';
 import contact from './contact.png';
+import shan from './shan.png'; 
+import caillou from './caillou.jpg'; 
+import riviere from './riviere.jpg'; 
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-      {/* POur la nav bar */}
-      <section class="nav">
-        <br></br>
-      
-         <ul>
-            <li><a href="#presentation">Claude</a></li>
-            <li><a href="#competence">Martine</a></li>
-            <li><a href="#experience">Jeanne</a></li>
-          </ul>
+class Contact_info extends React.Component {
+  
+  
+  constructor(props) {
+    super(props); 
+    this.state = {
+      color: '#282c34',
+      switch: true,
+      lettre: 'white', 
+      button: 'white',
+      buttonL: 'black'
+    }; 
+  }
 
-      </section>
+  handleClick() {
+    if (this.state.switch){
+      this.setState({
+        color: 'grey',
+        lettre: 'black',
+        switch: false,
+        button:'black',
+        buttonL: 'white',
+      }); 
+    }
+    else{
+      this.setState({
+        color: '#282c34',
+        lettre: 'white',
+        switch: true,
+        button:'white',
+        buttonL: 'black',
+      }) ; 
+    }
+  }
 
-      <header className="App-header">
-       
-        <article className="App-info-ui"> 
-          
-          <img id="Contact" src={contact}  alt="contact" />
+  render(){
+
+    return (
+    
+      <article className="App-info-ui" style={{backgroundColor:this.state.color,color:this.state.lettre}} > 
+      <img id="Contact" src={this.props.machin.photo}  alt="contact" />
 
           <table>
             <tr>
               <td>
                 <p>
-                  Prénom : 
+                  Prénom : {this.props.machin.prenom}
                 </p>
               </td>
               <td>
                 <p>
-                  Nom : zefzefzefef
+                  Nom : {this.props.machin.nom}
                 </p>
               </td>
             </tr>
             <tr>
               <td>
                 <p>
-                  Date de naissance : 
+                  Date de naissance : {this.props.machin.date_naissance}
                 </p>
               </td>
             </tr>
-            
           </table>
-          <a href="#" className="btn button-box"> Change style </a> 
-        
+
+          <a href="#" style={{backgroundColor:this.state.button, color:this.state.buttonL}} className="btn button-box" onClick={ () => this.handleClick() }> Change style </a> 
        </article>
-          
-        <article className="super-button-box">
+    ); 
+  }
+}
+
+class Super_description extends React.Component {
+  render(){
+    return(
+      <article className="super-button-box">
           <p > 
             Haec et huius modi quaedam innumerabilia ultrix facinorum impiorum bonorumque praemiatrix aliquotiens operatur Adrastia atque utinam semper quam vocabulo duplici etiam Nemesim appellamus: ius quoddam sublime numinis efficacis, humanarum mentium opinione lunari circulo superpositum, vel ut definiunt alii, substantialis tutela generali potentia partilibus praesidens fatis, quam theologi veteres .
           </p>
           
           <a href="#" className="button-box-super"> C'est super ! 👍 </a>
 
-        </article>
-        
-      <br></br>
-      <br></br>
+      </article>
+    );
+  }
+}
 
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      profils:[
+        {
+          nom: "La Riviere" ,
+          prenom: "Mathilde" ,
+          date_naissance: "30/11/1998", 
+          photo: riviere 
+        },
+        {
+          nom: "La Colinne",
+          prenom: "Claude",
+          date_naissance: "24/11/1998",
+          photo: caillou
+        },
+        {
+          nom: "La Montagne",
+          prenom: "Jeanne",
+          date_naissance: "03/11/1998",
+          photo: shan
+        }
+      ],
+      current:
+        {
+          nom: 'none',
+          prenom: 'none',
+          date_naissance: 'none', 
+          photo: contact
+        }
+    };
+  }
+
+  refresh (current) {
+    this.setState({
+      current: current,
+    });
+  }
+
+  
+  render(){
+    let buffer = this.state.current; 
+
+    return (
+      <div className="App">
+
+        {/* Pour la nav bar */}
+        <br></br>
+        <section class="nav">      
+        <ul>
+          <li><a onClick={ () => this.refresh(this.state.profils[0] )} > {this.state.profils[0].prenom} </a></li>
+          <li><a onClick={ () => this.refresh(this.state.profils[1] )} > {this.state.profils[1].prenom}</a></li>
+          <li><a onClick={ () => this.refresh(this.state.profils[2] )} > {this.state.profils[2].prenom}</a></li>
+          {/* <li><a > {this.state.current.prenom}</a></li> */}
+        </ul>
+        </section>
+          
+        <header className="App-header">
+        <Contact_info
+          machin={buffer}
+        >
+        </Contact_info> 
+        
+            
+        <Super_description> </Super_description>
+          
+        <br></br>
+        <br></br>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
